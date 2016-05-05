3205057a691e91ad8268c90ddb7a3984b87f9a2a
Phil <0f9234u9f0jw3@gmx.de>
2016-05-05 23:48:16 +0200

    added a few todos

--- VocabularyCollector/main.cpp
+++ VocabularyCollector/main.cpp
@@ -2,6 +2,15 @@
 #include "vocabularycollector.h"
 #include <QtWidgets/QApplication>
 
+/*
+    TODO:
+         remove items from list
+         make exporting to file prettier
+         make printing prettier
+         import from file
+         require http lookup to be enabled specifically
+*/
+
 int main(int argc, char *argv[]) {
     QApplication application{ argc, argv };
     VocabularyCollector window{ };
